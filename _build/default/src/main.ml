6acc87bfc08e361712dea87dd05821d1e4007126
chuyck <chuyck@buffalo.edu>
2025-11-09 10:04:33 -0500

    REPL looping to read input and translate to lam calc

--- _build/default/src/main.ml
+++ _build/default/src/main.ml
@@ -12,21 +12,30 @@ let rec list_to_string lst : string =
   | [x] -> x
   | x :: xs -> x ^ " " ^ list_to_string xs
 
-let read_input () =
-  print_endline "Enter OCaml Code to Convert to Lambda Calculus: ";
-  flush stdout; (* ensure the prompt shows up *)
+let rec read_input () =
+  print_string "Enter OCaml Code to Convert to Lambda Calculus (or type QUIT to exit): ";
+  flush stdout;  (* ensure prompt appears *)
+
   let code = read_line () in
+  if String.uppercase_ascii code = "QUIT" then (
+    print_endline "Exiting... REPL reduced to (λf.λx.x).";
+  ) else if String.uppercase_ascii code = "FUN" then (
+    print_endline "This project is inspired by Alonzo Church's Lambda Calculus. Church received an honorary degree from the University at Buffalo in 1990!";
+    read_input ()
+  ) else (
+    (* Tokenize *)
+    let tokens : string list = Church_translator.tokenize code in
+    let printed_tokens = list_to_string tokens in
+    print_endline ("You Entered: " ^ printed_tokens);
 
-  let tokens : string list = Church_translator.tokenize code in
-  let printed_tokens = list_to_string tokens in
-  print_endline ("You Entered: " ^ printed_tokens);
-  let parsed = Church_translator.parse_expr tokens in
-  let lam_string = lam_list_to_string parsed in
-  print_endline ("Lambda Calculus Representation: " ^ lam_string);
+    (* Parse *)
+    let parsed = Church_translator.parse_expr tokens in
+    let lam_string = lam_list_to_string parsed in
+    print_endline ("Lambda Calculus Representation: " ^ "(" ^ lam_string ^ ")");
+    print_endline "----";
 
-  (*print_endline "----";
-  print_endline ("Enter more OCaml Code to Convert to Lambda Calculus: ");*)
-  code
-;;
+    (* loop again *)
+    read_input ()
+  );;
 
 read_input ();;
