5599a7d69f1f8a48a32b36019e85010c0196d068
chuyck <chuyck@buffalo.edu>
2025-11-09 11:25:36 -0500

    repl working with error handling, looping input, and quit

--- src/main.ml
+++ src/main.ml
@@ -1,43 +1,43 @@
 open Lambda_lib
 
+(* Helper: print a list of strings *)
 let rec list_to_string lst : string =
   match lst with
   | [] -> ""
   | [x] -> x
   | x :: xs -> x ^ " " ^ list_to_string xs
 
-let rec read_input () =
+(* Main REPL loop *)
+let rec repl () =
   print_string "Enter OCaml Code to Convert to Lambda Calculus (or type QUIT to exit): ";
   flush stdout;
 
-  let code = read_line () in
-  if String.uppercase_ascii code = "QUIT" then (
-    print_endline "Exiting... REPL reduced to (λf.λx.x).";
-  ) else if String.uppercase_ascii code = "FUN" then (
-    print_endline "This project is inspired by Alonzo Church's Lambda Calculus. Fun Fact: Church received an honorary degree from the University at Buffalo in 1990!";
-    read_input ()
-  ) else (
-    try
-      (* Tokenize *)
-      let tokens : string list = Church_translator.tokenize code in
-      let printed_tokens = list_to_string tokens in
-      print_endline ("You Entered: " ^ printed_tokens);
+  let input = read_line () in
+  let input_upper = String.uppercase_ascii input in
 
-      (* Parse *)
-      let parsed = Church_translator.parse_expr tokens in
-      let lam_string = Church_lambda.to_lam_string parsed in
-      print_endline ("Lambda Calculus Representation: " ^ lam_string);
-      print_endline "----";
+  match input_upper with
+  | "QUIT" ->
+      print_endline "Exiting REPL. Reduced to (λf.λx.x)."
+  | "FUN" ->
+      print_endline "This project is inspired by Alonzo Church's Lambda Calculus. Fun Fact: Church received an honorary degree from the University at Buffalo in 1990!";
+      repl ()  (* continue looping *)
+  | _ ->
+      (* Try to tokenize, parse, and print *)
+      (try
+         let tokens = Church_translator.tokenize input in
+         print_endline ("You Entered: " ^ list_to_string tokens);
 
-    with
-    | Failure msg ->
-        (* Catch parse errors *)
-        Printf.printf "Error: %s\n----\n" msg
-    | _ ->
-        (* Catch any other unexpected exceptions *)
-        Printf.printf "An unexpected error occurred.\n----\n"
-  );
-  (* Loop again in all cases *)
-  read_input ();;
+         let parsed = Church_translator.parse_expr tokens in
+         let lam_string = Church_lambda.to_lam_string parsed in
+         print_endline ("Lambda Calculus Representation: " ^ lam_string);
+         print_endline "----"
+       with
+       | Failure msg ->
+           Printf.printf "Error: %s\n----\n" msg
+       | exn ->
+           Printf.printf "An unexpected error occurred: %s\n----\n" (Printexc.to_string exn)
+      );
+      repl ()  (* loop again *)
 
-read_input ();;
+(* Start REPL *)
+let () = repl ()
